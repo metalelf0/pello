3a9f7cceabce9813db5c94943ab83be6cd2bc327
Andrea Schiavini <metalelf0@gmail.com>
2021-11-28 10:37:13 +0100

    ♻️ Sort & align assignments

--- lib/pello/config.rb
+++ lib/pello/config.rb
@@ -1,20 +1,20 @@
 module Pello
   class Config
     CONFIG_FILE_PATH = "#{ENV['HOME']}/.config/pello/pello.yaml".freeze
-    attr_accessor :board_url, :list_name, :log_file, :username, :member_token, :developer_public_key
+    attr_accessor :board_url, :developer_public_key, :list_name, :log_file, :member_token, :username
 
     def initialize
-      config         = YAML.safe_load File.open(CONFIG_FILE_PATH).read
+      config = YAML.safe_load File.open(CONFIG_FILE_PATH).read
 
-      auth_config = config['auth']
+      auth_config           = config['auth']
       @developer_public_key = auth_config['developer_public_key']
-      @member_token = auth_config['member_token']
+      @member_token         = auth_config['member_token']
 
-      pello_config   = config['config']
-      @username  = pello_config['username']
-      @board_url = pello_config['board_url']
-      @list_name = pello_config['list_name']
-      @log_file  = pello_config['log_file']
+      pello_config          = config['config']
+      @username             = pello_config['username']
+      @board_url            = pello_config['board_url']
+      @list_name            = pello_config['list_name']
+      @log_file             = pello_config['log_file']
     end
   end
 end
